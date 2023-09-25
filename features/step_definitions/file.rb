0db79a1eb08ac2516605f779a5445de7fb4ccce6
Eugen Mayer <136934+EugenMayer@users.noreply.github.com>
2023-09-25 09:06:26 +0200

    Fix ruby 3.2 compatibility (#2)

--- features/step_definitions/file.rb
+++ features/step_definitions/file.rb
@@ -1,7 +1,7 @@
 require 'pp'
 
 Then(/^an absolute file named "(.+)" should exist$/) do |file|
-  File.exists?(file)
+  File.exist?(file)
 end
 
 When(/^I have made the file "(.+)" executable"$/) do |file|
